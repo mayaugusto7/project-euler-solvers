4f68a4e19b29ea5c0a6a96cd5f9b63bae61c0d69
mayribeiro <maycon_ribeiro@hotmail.com>
2017-01-04 21:53:00 -0200

    problem 10 ref

--- src/Problem10.java
+++ src/Problem10.java
@@ -1,6 +1,4 @@
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by MayconRibeiro on 21/12/16.
@@ -9,35 +7,13 @@ public class Problem10 {
 
     private static final long LIMIT = 2000000;
 
-    private static long total = 0;
-
     public static void main(String[] args) {
-
-       /**
-       long inicio = 0;
-       long fim = 200;
-
-       Thread[] threads = new Thread[10000];
-
-       for (int i = 0; i < threads.length; i++) {
-
-           PrimosThread primosThread = new PrimosThread(inicio, fim);
-           threads[i] = new Thread(primosThread);
-           threads[i].start();
-
-           total += primosThread.getSoma();
-           inicio = fim + 1;
-           fim += 200;
-       }
-
-        System.out.println("Total soma primos até 2 milhões " + total); **/
-
 
         BigInteger sum = new BigInteger("2");
 
         boolean isPrime = true;
 
-        for (int i = 3; i < 2000000; i++) {
+        for (int i = 3; i < LIMIT; i++) {
 
             double aa = Math.sqrt((double)i);
             
